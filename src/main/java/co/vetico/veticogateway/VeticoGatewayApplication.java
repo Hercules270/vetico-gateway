f68335cd8e1e9ae05183883fd0d4b2f4a7f2fa49
Irakli <ibezh17@freeuni.edu.ge>
2024-11-27 00:38:59 +0400

    initial commit

---
+++ src/main/java/co/vetico/veticogateway/VeticoGatewayApplication.java
@@ -0,0 +1,13 @@
+package co.vetico.veticogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeticoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeticoGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/vetico/veticogateway/VeticoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.vetico.veticogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeticoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
